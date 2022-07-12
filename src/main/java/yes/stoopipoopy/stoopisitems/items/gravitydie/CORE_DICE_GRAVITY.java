1c07ae47583a92086efaa7c6c5e0863e307c99aa
petropapahadjopoulos <anotherguywithabeanie@gmail.com>
2022-07-11 17:11:52 -0700

    testing abilitie is shiting

--- src/main/java/yes/stoopipoopy/stoopisitems/items/gravitydie/CORE_DICE_GRAVITY.java
+++ src/main/java/yes/stoopipoopy/stoopisitems/items/gravitydie/CORE_DICE_GRAVITY.java
@@ -1,11 +1,11 @@
-package yes.stoopipoopy.stoopisitems.items;
+package yes.stoopipoopy.stoopisitems.items.gravitydie;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-public class CORE_DICE_WARP implements Listener {
+public class CORE_DICE_GRAVITY implements Listener {
     @EventHandler
     public void onInteract(PlayerInteractEvent event) {
         if (event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_AIR) {
